517696327243ac143acdf2b10b55bfa49756f928
laynexx <argo150808@gmail.com>
2025-04-05 15:12:42 +0300

    fix: fixed bd connection

--- pkg/db/postgres/postgres.go
+++ pkg/db/postgres/postgres.go
@@ -8,15 +8,18 @@ import (
 )
 
 type PgConfig struct {
-	Host     string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
-	Port     uint16 `yaml:"POSTGRES_PORT" env:"POSTGRES_PORT" env-default:"5432"`
-	Username string `yaml:"POSTGRES_USER" env:"POSTGRES_USER" env-default:"root"`
-	Password string `yaml:"POSTGRES_PASSWORD" env:"POSTGRES_PASSWORD" env-default:"1234"`
-	Database string `yaml:"POSTGRES_DB" env:"POSTGRES_DB" env-default:"postgres"`
+	Host         string `yaml:"POSTGRES_HOST" env:"POSTGRES_HOST" env-default:"localhost"`
+	Port         uint16 `yaml:"POSTGRES_PORT" env:"POSTGRES_PORT" env-default:"5432"`
+	Username     string `yaml:"POSTGRES_USER" env:"POSTGRES_USER" env-default:"root"`
+	Password     string `yaml:"POSTGRES_PASSWORD" env:"POSTGRES_PASSWORD" env-default:"1234"`
+	Database     string `yaml:"POSTGRES_DB" env:"POSTGRES_DB" env-default:"postgres"`
+	MinConns     int32  `yaml:"POSTGRES_MIN_CONNS" env:"POSTGRES_MIN_CONNS" env-default:"5"`
+	MaxConns     int32  `yaml:"POSTGRES_MAX_CONNS" env:"POSTGRES_MAX_CONNS" env-default:"10"`
+	SearchSchema string `yaml:"POSTGRES_MAIN_SCHEMA" env:"POSTGRES_MAIN_SCHEMA" env-default:"public"`
 }
 
 func NewPostgres(c PgConfig) (*sql.DB, error) {
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disabled",
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
 		c.Username,
 		c.Password,
 		c.Host,
