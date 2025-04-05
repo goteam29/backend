5160c4cd150d12303c235c0afec7dbf73461c8dd
laynexx <argo150808@gmail.com>
2025-04-05 14:55:54 +0300

    fix: fixed main

--- cmd/main.go
+++ cmd/main.go
@@ -2,45 +2,36 @@ package main
 
 import (
 	mainConfig "api-repository/internal/config"
+	service2 "api-repository/internal/user-sevice/service"
 	userservice "api-repository/pkg/api/user-service"
 	"api-repository/pkg/db/postgres"
 	"api-repository/pkg/db/redis"
 	logger "api-repository/pkg/utils"
 	"context"
 	"fmt"
+	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"strconv"
+
+	"log"
 	"net"
 	"os"
 	"os/signal"
 )
 
-type UserService struct {
-	userservice.UnimplementedUserServer
-}
+func main() {
 
-func New() *UserService {
-	return &UserService{}
-}
-
-func (u *UserService) Get(ctx context.Context, req *userservice.Request) (*userservice.Reply, error) {
-	if req.Message == "" {
-		return nil, status.Errorf(codes.InvalidArgument, "request cannot be empty")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("can't get .env vars")
 	}
 
-	return &userservice.Reply{Message: req.Message}, nil
-}
-
-const port = "50050"
-
-func main() {
 	ctx := context.Background()
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
 	defer stop()
 
-	ctx, err := logger.New(ctx)
+	ctx, err = logger.New(ctx)
 	if err != nil {
 		logger.GetLoggerFromContext(ctx).Fatal(ctx, "can't initialize logger", zap.Error(err))
 	}
@@ -61,16 +52,16 @@ func main() {
 	redisConn := redis.NewRedisConn(config.RedisConf)
 	logger.GetLoggerFromContext(ctx).Info(ctx, "redis", zap.Any("redis", redisConn))
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", config.UserServicePort))
 	if err != nil {
 		logger.GetLoggerFromContext(ctx).Fatal(ctx, "failed to listen", zap.Error(err))
 	}
 
 	server := grpc.NewServer()
-	service := New()
-	userservice.RegisterUserServer(server, service)
+	svc := service2.NewUserService(pgConn)
+	userservice.RegisterUserServer(server, svc)
 
-	logger.GetLoggerFromContext(ctx).Info(ctx, "server started", zap.String("port", port))
+	logger.GetLoggerFromContext(ctx).Info(ctx, "server started", zap.String("port", strconv.Itoa(config.UserServicePort)))
 	go func() {
 		if err := server.Serve(lis); err != nil {
 			logger.GetLoggerFromContext(ctx).Fatal(ctx, "failed to serve", zap.Error(err))
---
+++ internal/user-sevice/service/service.go
@@ -0,0 +1,39 @@
+package service
+
+import (
+	"api-repository/internal/user-sevice/service/internal/handlers"
+	userservice "api-repository/pkg/api/user-service"
+	"context"
+	"database/sql"
+	"os"
+	"sync"
+)
+
+var once sync.Once
+
+type UserService struct {
+	userservice.UnimplementedUserServer
+	authHandler *handlers.AuthHandler
+	pgConn      *sql.DB
+}
+
+func NewUserService(pc *sql.DB) *UserService {
+	secret := os.Getenv("JWT_SECRET_TOKEN")
+
+	var s *UserService
+	once.Do(func() {
+		s = &UserService{
+			pgConn:      pc,
+			authHandler: handlers.NewAuthHandler(pc, secret),
+		}
+	})
+	return s
+}
+
+func (us *UserService) Register(ctx context.Context, request *userservice.RegisterRequest) (*userservice.RegisterResponse, error) {
+	return us.authHandler.Register(request)
+}
+
+func (us *UserService) Login(ctx context.Context, request *userservice.LoginRequest) (*userservice.LoginResponse, error) {
+	return us.authHandler.Login(request)
+}
