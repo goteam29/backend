cc290dedcb688495a06d439835ae64dad2502a1b
Ilya <faerwur@gmail.com>
2025-04-08 23:23:26 +0300

    implemented authorization and refactor AuthHandler to use JWT Manager

--- internal/services/user-service/service/internal/handlers/auth-handler.go
+++ internal/services/user-service/service/internal/handlers/auth-handler.go
@@ -2,27 +2,32 @@ package handlers
 
 import (
 	userservice "api-repository/pkg/api/user-service"
+	jwtManager "api-repository/pkg/utils"
 	"context"
 	"database/sql"
 	"errors"
 	"fmt"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
 
+const expirationTime = 7 * 24 * time.Hour
+
 type AuthHandler struct {
 	db     *sql.DB
 	secret string
+	jwt    *jwtManager.JWTManager
 }
 
 func NewAuthHandler(_db *sql.DB, _secret string) *AuthHandler {
+	JWTManager := jwtManager.NewJWTManager(_secret, expirationTime)
 	return &AuthHandler{
 		db:     _db,
 		secret: _secret,
+		jwt:    JWTManager,
 	}
 }
 
@@ -31,38 +36,25 @@ func (a *AuthHandler) Register(ctx context.Context, req *userservice.RegisterReq
 		return nil, errors.New("passwords don't match")
 	}
 
-	id := uuid.New()
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"name":   req.Username,
-		"userid": id,
-		"exp":    time.Now().Add(time.Hour * 24 * 7).Unix(),
-		"iat":    time.Now().Unix(),
-	})
-
-	token, err := claims.SignedString([]byte(a.secret))
+	id, token, err := a.jwt.Generate(req)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to generate token: %w", err)
 	}
 
-	userRow := a.db.QueryRow("SELECT id FROM users WHERE email = $1", req.Email)
-
-	err = userRow.Scan(&id)
+	_, err = a.db.Exec("INSERT INTO users (id, username, email, token) VALUES($1, $2, $3, $4)",
+		id,
+		req.Username,
+		req.Email,
+		token,
+	)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			_, err = a.db.Exec("INSERT INTO users VALUES($1, $2, $3, $4)",
-				id,
-				req.Username,
-				req.Email,
-				token,
-			)
-			if err != nil {
-				return nil, fmt.Errorf("failed to insert user: %w", err)
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				return nil, fmt.Errorf("user with this email already exists")
 			}
 		} else {
-			return nil, fmt.Errorf("failed to query user: %w", err)
+			return nil, fmt.Errorf("failed to insert user into database: %w", err)
 		}
-	} else {
-		return nil, errors.New("user already exists")
 	}
 
 	err = grpc.SendHeader(ctx, metadata.Pairs(
@@ -73,13 +65,30 @@ func (a *AuthHandler) Register(ctx context.Context, req *userservice.RegisterReq
 	}
 
 	return &userservice.RegisterResponse{
-		Uuid:    id.String(),
-		IsAdmin: false,
+		Uuid: "user successfully registered",
 	}, nil
 }
 
 func (a *AuthHandler) Login(ctx context.Context, req *userservice.LoginRequest) (*userservice.LoginResponse, error) {
-	return &userservice.LoginResponse{
-		Uuid: "1234",
-	}, nil
+	user := a.db.QueryRow("SELECT token FROM users WHERE email = $1;", req.Email)
+	var token string
+
+	if err := user.Scan(&token); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user with this email doesn't exist")
+		}
+		return nil, fmt.Errorf("login: failed to scan user: %w", err)
+	}
+
+	claims, err := a.jwt.Verify(token)
+	if err != nil {
+		return nil, fmt.Errorf("failed to verify token: %w", err)
+	} else {
+		if claims.Password != req.Password {
+			return nil, fmt.Errorf("wrong password")
+		}
+	}
+
+	res := &userservice.LoginResponse{Uuid: "account logged in"}
+	return res, nil
 }
