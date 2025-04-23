c880673f70e0e7ca3bdf73e88e03b542bc8975ab
laynexx <argo150808@gmail.com>
2025-04-23 12:01:36 +0300

    feat: added timeout fault tolerance pattern

--- internal/adapters/interceptors/logging-interceptor.go
+++ internal/adapters/interceptors/logging-interceptor.go
@@ -2,6 +2,7 @@ package interceptors
 
 import (
 	"context"
+	"errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
@@ -18,13 +19,22 @@ func LoggingInterceptor(l *zap.SugaredLogger) grpc.UnaryServerInterceptor {
 		t := time.Now()
 		l.Logf(0, "\n Request: %s | Time: %s \n FullInfo %v", info.FullMethod, t, info)
 
+		var cancel context.CancelFunc
+
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+
 		resp, err := handler(ctx, req)
 
 		if err != nil {
+			if errors.Is(err, context.DeadlineExceeded) {
+				l.Logf(zapcore.ErrorLevel, "Timeout reached")
+			}
 			l.Logf(zapcore.ErrorLevel, "Response: %s | Duration: %s | Err: %v", resp, time.Since(t), err)
+			return resp, err
 		} else {
 			l.Logf(zapcore.InfoLevel, "Response: %s | Duration: %s | Successfully completed ", resp, time.Since(t))
+			return resp, err
 		}
-		return resp, err
 	}
 }
